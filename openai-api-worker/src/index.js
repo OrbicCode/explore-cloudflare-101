42ecd00826161d952ec6e5713a2f9ac27d8190bd
James O'Kane <orbiccode@gmail.com>
2025-11-25 19:48:16 +0000

    updated cors

--- openai-api-worker/src/index.js
+++ openai-api-worker/src/index.js
@@ -1,7 +1,7 @@
 import Openai from 'openai';
 
 const corsHeaders = {
-	'Access-Control-Allow-Origin': 'http://localhost:5173',
+	'Access-Control-Allow-Origin': 'explore-cloudflare-101.pages.dev',
 	'Access-Control-Allow-Methods': 'POST, OPTIONS',
 	'Access-Control-Allow-Headers': 'Content-Type',
 };
